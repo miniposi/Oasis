ba62708fee26b00969545c59dea2fd59d36e8c26
miniposi <rndtn6851@gmail.com>
2024-04-14 12:38:40 +0900

    feat: custom data

---
+++ data/CustomData.ts
@@ -0,0 +1,51 @@
+interface CustomDataProps {
+	id: number;
+	src: string;
+	name: string;
+}
+
+export const CustomCatData: CustomDataProps[] = [
+	{
+		id: 1,
+		src: 'cat1.png',
+		name: '코리안 숏헤어',
+	},
+	{
+		id: 2,
+		src: 'cat2.png',
+		name: '샴',
+	},
+	{
+		id: 3,
+		src: 'cat3.png',
+		name: '랙돌',
+	},
+	{
+		id: 4,
+		src: 'cat4.png',
+		name: '유니크',
+	}
+];
+
+export const CustomDogData: CustomDataProps[] = [
+	{
+		id: 1,
+		src: 'dog1.png',
+		name: '말티즈',
+	},
+	{
+		id: 2,
+		src: 'dog2.png',
+		name: '유니크',
+	},
+	{
+		id: 3,
+		src: 'dog3.png',
+		name: '푸들',
+	},
+	{
+		id: 4,
+		src: 'dog4.png',
+		name: '슈나우저',
+	}
+];
