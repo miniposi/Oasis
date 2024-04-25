de4b7e4b9fadf823e12ca17fb72b686f919ec72c
miniposi <rndtn6851@gmail.com>
2024-04-25 16:18:27 +0900

    feat: add session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "styled-components";
 import type { AppProps } from "next/app";
 import theme from "@/styles/theme";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </ThemeProvider>
   );
 }
