8f6bef8fa2d376904ab9ab89e5c5206abbba0a88
miniposi <rndtn6851@gmail.com>
2024-04-25 16:15:41 +0900

    feat: myScrap Page

--- pages/myScrap.tsx
+++ pages/myScrap.tsx
@@ -1,29 +1,78 @@
 import Header from "@/components/Header";
 import StarRating from "@/components/StarRating";
 import styled from "styled-components";
+import getScrapList from "./api/getScrapList";
+import deleteScrap from "./api/deleteScrap";
+import { useEffect, useState } from "react";
+import getUser from "./api/getUser";
+import useNavigation from "@/hooks/useNavigation";
 
 function MyScrapPage() {
+  const [isUpdate, setIsUpdate] = useState(1);
+  const [scrapList, setScrapList] = useState([]);
+  const [userInfo, setUserInfo] = useState<any>();
+  const handleNavigation = useNavigation();
+
+  const handleDelete = async (id: any) => {
+    const response: any = await deleteScrap(id);
+    console.log(response);
+    setIsUpdate(isUpdate * -1);
+  };
+
+  const fetch = async () => {
+    const response: any = await getScrapList();
+    setScrapList(response.data);
+    const result: any = await getUser();
+    setUserInfo(result.data.user);
+  };
+
+  useEffect(() => {
+    fetch();
+  }, [isUpdate]);
+
   return (
     <>
-      <Header>포시의 서랍</Header>
+      <Header>{userInfo !== undefined ? userInfo.name : ""}의 서랍</Header>
       <StyledWrapper>
         <StyledLeftWrapper>
-          <StyledImg src="custom/cat1.png" alt="프로필 사진" />
-          포시
-          <StyledType>먼치킨</StyledType>
+          <StyledImg
+            src={
+              userInfo !== undefined
+                ? `http://14.39.203.129:13000/${userInfo.profilePic}`
+                : ""
+            }
+            alt="프로필 사진"
+          />
+          {userInfo !== undefined ? userInfo.name : ""}
+          <StyledType>
+            {userInfo !== undefined ? userInfo.breed : ""}
+          </StyledType>
         </StyledLeftWrapper>
         <StyledRightWrapper>
-          <StyledContent>
-            <StyledContentImg src="shop/adultdog.png" alt="상품 이미지" />
-            <StyledInfo>
-              <StyledName>
-                고양이섬 리필 50종 고양이섬 리필 50종 고양이섬 리필 50종
-                고양이섬 리필 50종
-              </StyledName>
-              <StyledPrice>1200원</StyledPrice>
-              <StarRating rating={3.0} />
-            </StyledInfo>
-          </StyledContent>
+          {scrapList.map((item: any) => (
+            <StyledContent key={item.id}>
+              <StyledDeleteButton onClick={() => handleDelete(item.Product.id)}>
+                <img
+                  src="icon/deleteicon.png"
+                  width={30}
+                  height={30}
+                  alt="삭제 버튼"
+                />
+              </StyledDeleteButton>
+              <StyledContentImg
+                onClick={() =>
+                  handleNavigation(`product?id=${item.Product.id}`)
+                }
+                src={item.Product.thumbnailUrl}
+                alt="상품 이미지"
+              />
+              <StyledInfo>
+                <StyledName>{item.Product.name}</StyledName>
+                <StyledPrice>{item.Product.price}원</StyledPrice>
+                <StarRating rating={item.Product.avgScore} />
+              </StyledInfo>
+            </StyledContent>
+          ))}
         </StyledRightWrapper>
       </StyledWrapper>
     </>
@@ -73,6 +122,7 @@ const StyledType = styled.p`
 const StyledContent = styled.div`
   width: 260px;
   height: 400px;
+  position: relative;
 `;
 
 const StyledContentImg = styled.img`
@@ -92,10 +142,19 @@ const StyledInfo = styled.div`
 
 const StyledName = styled.div`
   width: 240px;
-  height: 32px;
+  max-height: 70px;
   overflow: auto;
 `;
 
 const StyledPrice = styled.p`
   font-size: 16px;
+`;
+
+const StyledDeleteButton = styled.button`
+  background: transparent;
+  border: 0;
+  display: inline-block;
+  position: absolute;
+  right: 10px;
+  top: 10px;
 `;
