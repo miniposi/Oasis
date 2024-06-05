f5ac0b803cd0eb36b0b02b266084b2fc14f6f88d
miniposi <rndtn6851@gmail.com>
2024-06-05 16:04:07 +0900

    fix: save product state

--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -1,6 +1,6 @@
 import { useSearchParams } from "next/navigation";
 import styled from "styled-components";
-import Header from "@/components/Header";
+import Header from "@/components/Header/Header";
 import StarRating from "@/components/StarRating";
 import HorizontalLine from "@/components/HorizontalLine";
 import useNavigation from "@/hooks/useNavigation";
@@ -8,16 +8,18 @@ import Modal from "@/components/Modal";
 import { useEffect, useState } from "react";
 import getProduct from "@/api/getProduct";
 import postScrap from "@/api/postScrap";
+import { productState } from "@/hooks/getProduct";
+import { useRecoilState } from "recoil";
 
 interface ButtonProps {
   $design: "fill" | "unfill";
 }
 
 function ProductPage() {
-  const params = useSearchParams();
+  const id = useSearchParams().get("id");
   const handleNavigation = useNavigation();
   const [showModal, setShowModal] = useState(false);
-  const [productInfo, setProductInfo] = useState<any>({});
+  const [product, setProduct] = useRecoilState(productState);
 
   const handleScrap = () => {
     setShowModal(true);
@@ -27,60 +29,70 @@ function ProductPage() {
     post();
   };
 
-  const fetch = async () => {
-    const response: any = await getProduct(params.get("id"));
-    setProductInfo(response.data.product);
-  };
+  async function handleProduct() {
+    try {
+      const response: any = await getProduct(id);
+      setProduct(response.data.product);
+    } catch (error) {
+      alert("해당 제품의 정보를 가져올 수 없어요!");
+      handleNavigation(`product?id=${id}`);
+    }
+  }
 
   const post = async () => {
-    const response: any = await postScrap(params.get("id"));
+    const response: any = await postScrap(id);
     console.log(response);
   };
 
   useEffect(() => {
-    fetch();
+    handleProduct();
   }, []);
 
   return (
     <>
       <Header>제품 정보</Header>
       <StyledWrapper>
-        <StyledImg src={productInfo.thumbnailUrl} alt="제품 이미지" />
+        <StyledImg
+          src={product.thumbnailUrl}
+          alt="제품 이미지"
+        />
         <StyledInnerWrapper>
           <StyledSource>
-            오아시스 상점 {">"} {productInfo.pcg} {">"} {productInfo.scg} {">"}{" "}
-            {productInfo.dcg}
+            오아시스 상점 {">"} {product.pcg} {">"} {product.scg} {">"} {product.dcg}
           </StyledSource>
-          <StyledName>{productInfo.name}</StyledName>
-          <StyledPrice>{productInfo.price}원</StyledPrice>
+          <StyledName>{product.name}</StyledName>
+          <StyledPrice>{product.price}원</StyledPrice>
           <StyledRatingWrapper>
-            {productInfo.avgScore}
-            <StarRating rating={productInfo.avgScore} />
+            {product.avgScore}
+            <StarRating rating={product.avgScore} />
           </StyledRatingWrapper>
           <HorizontalLine />
           <StyledButtonWrapper>
             <StyledButton
               type="button"
               $design="unfill"
-              onClick={() =>
-                handleNavigation(`productDetail?id=${params.get("id")}`)
-              }
+              onClick={() => handleNavigation(`productDetail?id=${id}`)}
             >
               상세보기
             </StyledButton>
             <StyledButton
               type="button"
               $design="unfill"
-              onClick={() =>
-                handleNavigation(`productReview?id=${params.get("id")}`)
-              }
+              onClick={() => handleNavigation(`review?id=${id}`)}
             >
               후기보기
             </StyledButton>
-            <StyledButton type="button" $design="fill" onClick={handleScrap}>
+            <StyledButton
+              type="button"
+              $design="fill"
+              onClick={handleScrap}
+            >
               찜하기
             </StyledButton>
-            <Modal showModal={showModal} setShowModal={setShowModal} />
+            <Modal
+              showModal={showModal}
+              setShowModal={setShowModal}
+            />
           </StyledButtonWrapper>
         </StyledInnerWrapper>
       </StyledWrapper>
